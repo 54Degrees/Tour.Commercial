fb6fd270b344faec6795a355a962ecc844d0b8fb
Ellen Corcoran-Walsh <54degreesdesign@gmail.com>
2017-03-05 23:51:07 +0000

    Commercial Tour (SAMPLE)

---
+++ data.js
@@ -0,0 +1,305 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-tannyoky-guns-and-ammo",
+      "name": "Tannyoky Guns and Ammo",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": 0.03153622253111976,
+        "pitch": -0.04997507856852934,
+        "fov": 1.0678620569631962
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.49378217816507686,
+          "pitch": 0.08840514294556634,
+          "rotation": 0,
+          "target": "1-shop---clothing-range"
+        },
+        {
+          "yaw": 0.030545137702203462,
+          "pitch": -0.022756309890931803,
+          "rotation": 0,
+          "target": "2-shop---shooting-accessories"
+        },
+        {
+          "yaw": 0.6305140257465176,
+          "pitch": -0.3389377275563028,
+          "rotation": 1.5707963267948966,
+          "target": "3-shooting-range---down-the-line"
+        },
+        {
+          "yaw": 0.6324548954167213,
+          "pitch": -0.10921351399387369,
+          "rotation": 1.5707963267948966,
+          "target": "4-shooting-range---sports-shooting-"
+        },
+        {
+          "yaw": 0.6323514047467231,
+          "pitch": 0.11733907786882192,
+          "rotation": 1.5707963267948966,
+          "target": "5-shooting-range---skeet-shooting"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.08139023106397758,
+          "pitch": -0.515264227523268,
+          "title": "Tannyoky Guns and Ammo",
+          "text": "Welcome to Tannyoky Guns and Ammo.&nbsp;<div><br></div><div>Our state of the art shop and shooting range offer a wide range of services and goods for you to enjoy.</div><div><br></div><div>Please take a look around.</div><div><br></div><div>Opening Hours:</div><div>09:00 - 17:00 Tues to Sat</div>"
+        }
+      ]
+    },
+    {
+      "id": "1-shop---clothing-range",
+      "name": "Shop - Clothing Range",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": 0.8547224531662039,
+        "pitch": 0.2534278957474356,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.4826537762669503,
+          "pitch": 0.12838477302323348,
+          "rotation": 0,
+          "target": "0-tannyoky-guns-and-ammo"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.8182715250522818,
+          "pitch": -0.4281849212819502,
+          "title": "&nbsp;Clothing Shop",
+          "text": "<div>Main Clothing Accessories Shop with all the latest brands and products.<br></div><div><br></div><div>Opening Hours:</div><div>09:00 - 17:00 Tues - Sat</div>"
+        }
+      ]
+    },
+    {
+      "id": "2-shop---shooting-accessories",
+      "name": "Shop - Shooting Accessories",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": 1.008341053511657,
+        "pitch": 0.24173712428419947,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5620681136249832,
+          "pitch": 0.03249571017454578,
+          "rotation": 0,
+          "target": "0-tannyoky-guns-and-ammo"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.9579128045185961,
+          "pitch": -0.4364128081887326,
+          "title": "Shooting Accessories Shop",
+          "text": "<div>Main Shooting Accessories Shop with all the latest brands and products.</div><div><br></div><div>Opening Hours:</div><div>09:00 - 17:00 Tues - Sat</div>"
+        }
+      ]
+    },
+    {
+      "id": "3-shooting-range---down-the-line",
+      "name": "Shooting Range - Down The Line",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": -1.7071431380557378,
+        "pitch": 0.20800325191567026,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.6359748959784106,
+          "pitch": 0.036450216394477764,
+          "rotation": 0,
+          "target": "0-tannyoky-guns-and-ammo"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.7420102074536246,
+          "pitch": -0.4800166342791563,
+          "title": "Down The Line",
+          "text": "Down The Line shooting Range with 4 shooting locations.<div><br></div><div>Opening Hours:</div><div>09:00 - 17:00 Tues - Sat</div>"
+        }
+      ]
+    },
+    {
+      "id": "4-shooting-range---sports-shooting-",
+      "name": "Shooting Range - Sports Shooting ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": 3.001709686206768,
+        "pitch": 0.1659331004355149,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.83270888682733,
+          "pitch": 0.07483766574400263,
+          "rotation": 0,
+          "target": "0-tannyoky-guns-and-ammo"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.9888629736414485,
+          "pitch": -0.5386401291991021,
+          "title": "Sports Shooting&nbsp;",
+          "text": "<div>Premier Sports Shooting Range with 5 shooting locations.<br></div><div><br></div><div>Opening Hours:</div><div>09:00 - 17:00 Tue to Sat</div>"
+        }
+      ]
+    },
+    {
+      "id": "5-shooting-range---skeet-shooting",
+      "name": "Shooting Range - Skeet Shooting",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": 1.5835918002233678,
+        "pitch": 0.1275354494301979,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.373582607256722,
+          "pitch": 0.014436309202089603,
+          "rotation": 0,
+          "target": "0-tannyoky-guns-and-ammo"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.5594022029720644,
+          "pitch": -0.5931405337420053,
+          "title": "Skeet Shooting",
+          "text": "Premier Skeet Shooting Range with 7 shooting locations.<div><br></div><div>Opening Hours:</div><div>09:00 - 17:00 Tue to Sat</div>"
+        }
+      ]
+    }
+  ],
+  "name": "Tannyoky Guns and Ammo",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
